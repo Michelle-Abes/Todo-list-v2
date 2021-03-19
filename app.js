a3c92f36a5501b25fc2518ca392c47c2de369f21
Michelle Abraham <63426545+Michelle-Abes@users.noreply.github.com>
2021-03-19 21:58:18 +0530

    Add gitignore, procfile and ports

--- app.js
+++ app.js
@@ -157,51 +157,10 @@ app.post("/delete", (req,res) => {
     }
 });
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const date = require(__dirname + "/date.js");
+app.listen(port, () => console.log("Server has started succesfully."));
 
-// const app = express();
-
-// app.set('view engine', 'ejs');
-
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(express.static("public"));
-
-// const items = ["Buy Food", "Cook Food", "Eat Food"];
-// const workItems = [];
-
-// app.get("/", function(req, res) {
-
-// const day = date.getDate();
-
-//   res.render("list", {listTitle: day, newListItems: items});
-
-// });
-
-// app.post("/", function(req, res){
-
-//   const item = req.body.newItem;
-
-//   if (req.body.list === "Work") {
-//     workItems.push(item);
-//     res.redirect("/work");
-//   } else {
-//     items.push(item);
-//     res.redirect("/");
-//   }
-// });
-
-// app.get("/work", function(req,res){
-//   res.render("list", {listTitle: "Work List", newListItems: workItems});
-// });
-
-// app.get("/about", function(req, res){
-//   res.render("about");
-// });
-
-// app.listen(3000, function() {
-//   console.log("Server started on port 3000");
-// });
